b60c2799961c28b4ed79677e99a595c99379be10
John Roberts <john.calvin.roberts@gmail.com>
2017-12-12 10:47:51 +0800

    add commander

--- cli/cli.js
+++ cli/cli.js
@@ -2,11 +2,49 @@
 
 const fs = require('fs-extra')
 const path = require('path')
-const Vorpal = require('vorpal')
+const commander = require('commander')
+const chalk = require('chalk')
+const vorpal = require('vorpal')
+const log = console.log
 
-console.log('nothing here yet')
-
-module.exports = function(options) {
-  const vorpal = Vorpal()
-  const chalk = vorpal.chalk
+function initFido () {
+  log(chalk.green`
+          new fido project!!!🐶🐶
+          heheheh
+    `)
 }
+
+
+function generatePage (pageName) {
+  log(`generating a page named...${pageName}`)
+}
+
+function generateComp (compName) {
+  log(chalk.green(`generating a component named..${compName}`))
+}
+
+//declare commander format
+commander.usage('[command] <options ...>')
+
+commander.
+  command('init')
+  .description('add fido to your project')
+  .action(name => {
+    initFido()
+})
+commander
+  .command('page <pageName> <pagePreset>')
+  .description('Generate a new page from a fido preset')
+  .action(pageName => {
+  generatePage(pageName)
+})
+commander
+  .command('component <compName> <compPreset>')
+  .description('Generate a new page from a fido preset')
+  .action(compName => {
+  generateComp(compName)
+})
+
+
+commander.parse(process.argv)
+if (!commander.args.length) commander.help()
