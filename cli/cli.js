568478241dbd6179016f1caa20b03e32b3b4a749
John Roberts <john.calvin.roberts@gmail.com>
2017-12-13 10:50:02 +0800

    replace commander with vorpal

--- cli/cli.js
+++ cli/cli.js
@@ -2,80 +2,77 @@
 
 const fs = require('fs-extra')
 const path = require('path')
-const commander = require('commander')
 const chalk = require('chalk')
 const vorpal = require('vorpal')()
 const log = console.log
 
-const stylePresets = [{id: 1, name: 'goober', title: 'fucko'}, {id: 2, name: 'dober', title: 'fucko'}, {id: 3, name: 'freedom', title: 'fucko'}]
+const pagePresets = [{id: 1, name: 'goober', title: 'fucko'}, {id: 2, name: 'dober', title: 'fucko'}, {id: 3, name: 'freedom', title: 'fucko'}]
+const componentPresets = [{id: 1, name: 'goober', title: 'fucko'}, {id: 2, name: 'dober', title: 'fucko'}, {id: 3, name: 'freedom', title: 'fucko'}]
 
 
+
+
+//initialize a project
 vorpal
   .command('init')
   .description('Initialize a fido project.')
   .action(function(args, callback) {
+      log(chalk.green`
+              new fido project!!! 🐶 🐶
+              Let's generate the base styles first.
+        `)
     this.prompt({
-      type: 'list',
-      name: 'themeId',
-      choices: themes.map((theme) => ({
-        name: `${theme.title} | ${theme.name} | ${theme.id}`,
-        value: theme.id
-      })),
-      message: 'Choose theme to download'
+      type: 'input',
+      name: 'primaryHex',
+      message: 'Please enter primary hexcode for your project:_'
     })
     .then(result => log(result))
     .catch(error => chalk.error(log(error)))
   })
 
-function initFido () {
-  log(chalk.green`
-          new fido project!!! 🐶 🐶
-          hehehehe
-    `)
-  vorpal
-    .show()
-    .parse(process.argv)
-}
 
-
-function generatePage (pageName) {
-  log(`generating a page named...${pageName}`)
-}
-
-function generateComp (compName) {
-  log(chalk.green(`generating a component named..${compName}`))
-}
-
-//declare commander format
-commander
-  .version('0.0.1')
-  .usage('[command] <options ...>')
-
-
-//init command, for when adding fido to your project
-commander
-  .command('init')
-  .description('add fido to your project')
-  .action(name => {
-    initFido()
-})
-
-//Generate a page from a UI preset
-commander
+vorpal
   .command('page <pageName> <pagePreset>')
   .description('Generate a new page from a fido preset')
-  .action(pageName => {
-    generatePage(pageName)
+  .action(function(pageName) {
+    this.prompt({
+      type: 'list',
+      name: 'pagePreset',
+      choices: pagePresets.map((page) => ({
+        name: `${page.title} | ${page.name} | ${page.id}`,
+        value: page.id
+      })),
+      message: 'Please choose the page preset you want'
+    })
+    .then(result => log(result))
+    .catch(error => chalk.error(log(error)))
 })
 
-//Generate a component to add to your project
-commander
-  .command('component <compName> <compPreset>')
-  .description('Generate a new page from a fido preset')
-  .action(compName => {
-    generateComp(compName)
-})
+vorpal
+    .command('component <compName> <compPreset>')
+    .description('Generate a new component from a fido preset')
+    .action(function(compName) {
+      this.prompt({
+        type: 'list',
+        name: 'compPreset',
+        choices: componentPresets.map((style) => ({
+          name: `${comp.title} | ${comp.name} | ${comp.id}`,
+          value: comp.id
+        })),
+        message: 'Please choose the component preset you want'
+      })
+      .then(result => log(result))
+      .catch(error => chalk.error(log(error)))
+  })
 
 
-commander.parse(process.argv)
-if (!commander.args.length) commander.help()
+
+
+//parse the node TTY instance into vorpal
+vorpal
+  .version('0.0.1')
+  .description('Fido command line tool for wechat mini programs')
+  .delimiter('fido$')
+  .show()
+  .parse(process.argv)
+
